2eddaa00fc5edfca66bd44a9ddfa873cc72258aa
zerosoul <yanggc888@163.com>
2021-08-26 20:01:01 +0800

    feat: continue while first invite

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -6,17 +6,38 @@ import './ws-notification';
 // 安装扩展触发的事件
 chrome.runtime.onInstalled.addListener(function (details) {
   const { reason } = details;
+  console.log("install reason", reason);
   switch (reason) {
     case 'install':
     case 'update':
-    case 'chrome_update':
-      chrome.tabs.create(
-        {
-          active: true,
-          url: 'https://nicegoodthings.com/landing/webrowse'
-        },
-        null
-      );
+    case 'chrome_update': {
+      chrome.tabs.query({ url: "*://*/transfer/wb/*" }, function (tabs) {
+        console.log('query invite tabs', tabs);
+        if (!tabs || tabs.length === 0) {
+          chrome.tabs.create(
+            {
+              active: true,
+              url: 'https://nicegoodthings.com/landing/webrowse'
+            },
+            null
+          );
+        } else {
+          for (let i = 0; i < tabs.length; i++) {
+            const tab = tabs[i];
+            if (i == 0) {
+              chrome.tabs.executeScript(tab.id, {
+                file: 'catchInviteId.bundle.js'
+              }, () => {
+                console.log('catch script executed');
+                chrome.tabs.update(tab.id, { active: true });
+              });
+            } else {
+              chrome.tabs.remove(tab.id);
+            }
+          }
+        }
+      });
+    }
       break;
   }
 });
