6136f8e66ca6569bd81d1247f25d50e41cfc5b42
zerosoul <yanggc888@163.com>
2021-09-03 17:34:52 +0800

    refactor: popup UX

--- src/pages/Popup/components/WindowList.js
+++ src/pages/Popup/components/WindowList.js
@@ -5,7 +5,6 @@ import { EVENTS } from '../../../common'
 
 const StyledWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
   flex-direction: column;
   align-items: flex-start;
   padding:0 24px 16px 24px ;
@@ -15,111 +14,122 @@ const StyledWrapper = styled.div`
     line-height: 22px;
     color: #121212;
   }
-  .window{
-    width: 100%;
-    width:-webkit-fill-available;
+  >.block{
     display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
     flex-direction: column;
-    gap: 8px;
-    padding:0;
+    width: 326px;
+    max-height: 240px;
     background:#fff;
-    border-radius: 15px;
-    overflow: hidden;
-    margin-bottom: 10px;
-    .title{
-      display: flex;
+    border-radius: 10px;
+    padding:8px 0;
+    overflow: overlay;
+    &.empty{
       align-items: center;
-      gap: 8px;
-      cursor: pointer;
-      color: #000;
-      padding:10px 12px;
-      margin: 0;
-      .arrow{
-        width:20px;
-        height:20px;
-        border-radius: 50%;
-        background-color:#EAEAEA;
-        background-size: 16px;
-        background-image: url(${`chrome-extension://${chrome.runtime.id}/assets/icon/arrow.down.svg`});
-        background-repeat: no-repeat;
-        background-position: center;
-        transition: transform .5s ease-in;
+      justify-content: center;
+      height: 238px;
+      .tip{
+        text-align: center;
+        font-weight: normal;
+        font-size: 12px;
+        line-height: 16px;
+        color: #aaa;
+        padding:0 52px;
       }
-      .con{
-        font-weight: 600;
-        font-size: 14px;
-        line-height: 22px;
-        color: inherit;
-        white-space: nowrap;
-      }
-
-      .num{
-        padding:3px 6px;
-        border: 1px solid #757575;
-        border-radius: 15px;
-        font-size: 10px;
-        line-height: 13px;
-        color: #757575;
-      }
-
     }
-    .tabs{
-      margin: 0;
-      padding: 0 12px;
-      list-style: none;
-      display: none;
+    .window{
+      width:-webkit-fill-available;
+      display: flex;
       flex-direction: column;
       gap: 8px;
-      border-left: 2px solid #EAEAEA;
-      padding-left: 12px;
-      margin-left: 28px;
+      padding:0;
       margin-bottom: 10px;
-      .tab{
+      .title{
         display: flex;
         align-items: center;
-        cursor: pointer;
         gap: 8px;
-        img{
-          width:24px;
-          height:24px;
+        cursor: pointer;
+        color: #000;
+        padding:10px 12px;
+        margin: 0;
+        .arrow{
+          width:20px;
+          height:20px;
+          border-radius: 50%;
+          background-color:#EAEAEA;
+          background-size: 16px;
+          background-image: url(${`chrome-extension://${chrome.runtime.id}/assets/icon/arrow.down.svg`});
+          background-repeat: no-repeat;
+          background-position: center;
+          transition: transform .5s ease-in;
         }
         .con{
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          max-width: 300px;
-          overflow: hidden;
+          font-weight: 600;
           font-size: 14px;
-          line-height: 18px;
-          color: #000;
+          line-height: 22px;
+          color: inherit;
+          white-space: nowrap;
+        }
+
+        .num{
+          padding:3px 6px;
+          border: 1px solid #757575;
+          border-radius: 15px;
+          font-size: 10px;
+          line-height: 13px;
+          color: #757575;
+        }
+
+      }
+      .tabs{
+        margin: 0;
+        padding: 0 12px;
+        list-style: none;
+        display: none;
+        flex-direction: column;
+        gap: 8px;
+        border-left: 2px solid #EAEAEA;
+        padding-left: 12px;
+        margin-left: 28px;
+        margin-bottom: 10px;
+        .tab{
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+          gap: 8px;
+          img{
+            width:24px;
+            height:24px;
+          }
+          .con{
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            max-width: 300px;
+            overflow: hidden;
+            font-size: 14px;
+            line-height: 18px;
+            color: #000;
+          }
         }
       }
-    }
-    &.expand{
-      .tabs{
-        display: flex;
+      &.expand{
+        .tabs{
+          display: flex;
+        }
+        .title .arrow{
+          transform: rotate(180deg);
+        }
       }
-      .title .arrow{
-        transform: rotate(180deg);
-      }
-    }
-    &:hover{
-      background:#EBF3FE;
-      .title{
-        color:#fff;
-        background: #056CF2;
-        .num{
-          color: #DBE2EB;
-          border-color: #DBE2EB;
+      &:hover{
+        .title{
+          background: #E8F2FF;
         }
       }
     }
   }
 `;
-export default function WindowList({ windows = [{
-  roomId: 'dddd', roomName: "test", tabs: [{
-    id: 222, title: 'ceshi', windowId: 33333
-  }]
-}] }) {
+export default function WindowList({ windows = null }) {
   console.log("window list", windows);
   const handleJumpTab = ({ currentTarget }) => {
     const { tabId, windowId } = currentTarget.dataset;
@@ -128,29 +138,32 @@ export default function WindowList({ windows = [{
   const toggleExpand = ({ currentTarget }) => {
     currentTarget.parentElement.classList.toggle('expand')
   }
-  if (!windows || windows.length == 0) return null;
+  if (!windows) return null;
   return (
     <StyledWrapper>
       <h2 className="title">Active Windows</h2>
-      {windows.map(({ roomId, roomName, tabs }) => {
-        return <div key={roomId} className="window">
-          <h3 className="title" onClick={toggleExpand}>
-            <i className='arrow'></i>
-            <span className="con">
-              {roomName}
-            </span>
-            <span className="num">{tabs.length} tabs</span>
-          </h3>
-          <ul className="tabs">
-            {tabs.map(({ id, title, favIconUrl, windowId }) => {
-              return <li onClick={handleJumpTab} data-window-id={windowId} data-tab-id={id} key={id} title={title} className="tab">
-                <img src={favIconUrl || "https://files.authing.co/authing-console/default-user-avatar.png"} alt="favicon" />
-                <span className="con">{title}</span>
-              </li>
-            })}
-          </ul>
-        </div>
-      })}
+      <div className={`block ${windows.length == 0 ? 'empty' : ''}`}>
+        {windows.length == 0 && <div className="tip">You haven’t saved any windows yet. Start cobrowsing and save any window that you would like to share again!</div>}
+        {windows.map(({ roomId, roomName, tabs }) => {
+          return <div key={roomId} className="window">
+            <h3 className="title" onClick={toggleExpand}>
+              <i className='arrow'></i>
+              <span className="con">
+                {roomName}
+              </span>
+              <span className="num">{tabs.length} tabs</span>
+            </h3>
+            <ul className="tabs">
+              {tabs.map(({ id, title, favIconUrl, windowId }) => {
+                return <li onClick={handleJumpTab} data-window-id={windowId} data-tab-id={id} key={id} title={title} className="tab">
+                  <img src={favIconUrl || "https://files.authing.co/authing-console/default-user-avatar.png"} alt="favicon" />
+                  <span className="con">{title}</span>
+                </li>
+              })}
+            </ul>
+          </div>
+        })}
+      </div>
     </StyledWrapper>
   )
 }
