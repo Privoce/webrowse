57acbd293d45aefcfb1ea9969b2ec957ec61cebf
zerosoul <yanggc888@163.com>
2021-09-23 23:49:30 +0800

    refactor: remove unused code

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,53 +1,5 @@
 
 import './message-events';
 import './ws-workspace';
-
-// 安装扩展触发的事件
-const executeScripts = () => {
-  chrome.tabs.query({ active: false }, (tabs = []) => {
-    tabs.forEach(tab => {
-      chrome.tabs.reload(tab.id)
-    })
-  })
-}
-chrome.runtime.onInstalled.addListener(function (details) {
-  const { reason } = details;
-  console.log("install reason", reason);
-  switch (reason) {
-    case 'install': {
-      chrome.tabs.query({ url: "*://*/transfer/wb/*" }, function (tabs) {
-        console.log('query invite tabs', tabs);
-        if (!tabs || tabs.length === 0) {
-          chrome.tabs.create(
-            {
-              active: true,
-              url: 'https://webrow.se/guiding/'
-            }, () => {
-              executeScripts();
-            }
-          );
-        } else {
-          for (let i = 0; i < tabs.length; i++) {
-            const tab = tabs[i];
-            if (i == 0) {
-              chrome.tabs.executeScript(tab.id, {
-                file: 'catchInviteId.bundle.js'
-              }, () => {
-                console.log('catch script executed');
-                chrome.tabs.update(tab.id, { active: true });
-              });
-            } else {
-              chrome.tabs.remove(tab.id);
-            }
-          }
-          executeScripts()
-        }
-      });
-    }
-      break;
-  }
-});
-// case 'update':
-// case 'chrome_update':
 console.log('bg script ready~');
 
