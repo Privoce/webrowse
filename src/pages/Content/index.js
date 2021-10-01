241970949dea96501f283c89dc5ec811e737dd31
zerosoul <yanggc888@163.com>
2021-10-01 22:43:05 +0800

    feat: react-shadow

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+// import {useRef} from 'react';
 import ReactDOM from 'react-dom';
 import Webrowse from './Webrowse';
-
+import root from 'react-shadow/styled-components';
 const PanelID = 'PORTAL_WEBROWSE_PANEL';
 console.log('index.ext exe');
-let panel = document.createElement('aside');
+let panel = document.createElement('webrowse');
 panel.id = PanelID;
 document.body.appendChild(panel);
-ReactDOM.render(<Webrowse />, document.getElementById(PanelID));
+ReactDOM.render(
+  <root.div>
+    <Webrowse />
+  </root.div>, document.getElementById(PanelID));
