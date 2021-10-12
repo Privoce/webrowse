073185455a79f0d6be5df07e38c9100f6951fa60
zerosoul <yanggc888@163.com>
2021-10-12 17:27:28 +0800

    refactor: update to webpack cli 4

--- utils/webserver.js
+++ utils/webserver.js
@@ -2,52 +2,43 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 process.env.ASSET_PATH = '/';
-
-var WebpackDevServer = require('webpack-dev-server'),
+const PORT = 3002;
+const WebpackDevServer = require('webpack-dev-server'),
   webpack = require('webpack'),
   config = require('../webpack.config'),
-  env = require('./env'),
   path = require('path');
 
-var options = config.chromeExtensionBoilerplate || {};
-var excludeEntriesToHotReload = options.notHotReload || [];
+const options = config.chromeExtensionBoilerplate || {};
+const excludeEntriesToHotReload = options.notHotReload || [];
 
 for (var entryName in config.entry) {
   if (excludeEntriesToHotReload.indexOf(entryName) === -1) {
     config.entry[entryName] = [
-      'webpack-dev-server/client?http://localhost:' + env.PORT,
+      'webpack-dev-server/client?http://localhost:' + PORT,
       'webpack/hot/dev-server',
     ].concat(config.entry[entryName]);
   }
 }
 
-config.plugins = [new webpack.HotModuleReplacementPlugin()].concat(
-  config.plugins || []
-);
-
 delete config.chromeExtensionBoilerplate;
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 
-var server = new WebpackDevServer(compiler, {
-  stats: {
-    errorDetails: true
+const server = new WebpackDevServer({
+  static: {
+    directory: path.join(__dirname, '../build'),
   },
   https: false,
   hot: true,
-  injectClient: false,
-  writeToDisk: true,
-  port: env.PORT,
-  contentBase: path.join(__dirname, '../build'),
-  publicPath: `http://localhost:${env.PORT}`,
+  port: PORT,
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
-  disableHostCheck: true,
-});
+  allowedHosts: "all",
+  devMiddleware: {
+    publicPath: `http://localhost:${PORT}`,
+    writeToDisk: true,
+  },
+}, compiler);
 
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept();
-}
-
-server.listen(env.PORT);
+server.listen(PORT);
