a1f8547220b6a904d767a93efbadd1c4b81525da
zerosoul <yanggc888@163.com>
2021-12-17 21:47:47 +0800

    chore: remove unused functions

--- src/pages/Content/Webrowse/hooks/utils.js
+++ src/pages/Content/Webrowse/hooks/utils.js
@@ -1,32 +1,3 @@
-// import { sendMessageToBackground, MessageLocation } from '@wbet/message-api'
-
-// import { EVENTS } from '../../../../common';
-const selectText = (node) => {
-  // node = document.getElementById(node);
-  if (document.body.createTextRange) {
-    const range = document.body.createTextRange();
-    range.moveToElementText(node);
-    range.select();
-  } else if (window.getSelection) {
-    const selection = window.getSelection();
-    const range = document.createRange();
-    range.selectNodeContents(node);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  } else {
-    console.warn('Could not select text in node: Unsupported browser.');
-  }
-};
-function getUsername() {
-  return new Promise((resolve) => {
-    let arr = ['user', 'fakename'];
-    chrome.storage.sync.get(arr, (result) => {
-      let fake = typeof result.fakename !== 'undefined';
-      let name = fake ? result.fakename : result.user?.username || '';
-      resolve({ value: name, fake } || { value: null, fake: false });
-    });
-  });
-}
 function getUser() {
   return new Promise((resolve) => {
     let arr = ['user', 'fakename'];
@@ -36,11 +7,6 @@ function getUser() {
     });
   });
 }
-const preventCloseTabHandler = (evt) => {
-  evt.preventDefault();
-  evt.returnValue = 'Webrowse is still in connectiong, ary you sure to quit?';
-  // return 'Webrowse is still in connectiong, ary you sure to quit?';
-};
 
 function throttle(fn, interval = 200) {
   // 维护上次执行的时间
@@ -62,88 +28,6 @@ function throttle(fn, interval = 200) {
     }
   };
 }
-function getTranslateValues(element) {
-  const style = window.getComputedStyle(element);
-  const matrix = style['transform'] || style.webkitTransform || style.mozTransform;
-
-  // No transform property. Simply return 0 values.
-  if (matrix === 'none' || typeof matrix === 'undefined') {
-    return {
-      x: 0,
-      y: 0,
-      z: 0
-    };
-  }
-  // Can either be 2d or 3d transform
-  const matrixType = matrix.includes('3d') ? '3d' : '2d';
-  const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ');
-  // 2d matrices have 6 values
-  // Last 2 values are X and Y.
-  // 2d matrices does not have Z value.
-  if (matrixType === '2d') {
-    return {
-      x: matrixValues[4],
-      y: matrixValues[5],
-      z: 0
-    };
-  }
-  // 3d matrices have 16 values
-  // The 13th, 14th, and 15th values are X, Y, and Z
-  if (matrixType === '3d') {
-    return {
-      x: matrixValues[12],
-      y: matrixValues[13],
-      z: matrixValues[14]
-    };
-  }
-}
-function getVideoPlayer() {
-  let videos = Array.from(document.querySelectorAll('video')).filter((v) => !!v.src);
-  let largestVideoSize = 0;
-  let video = videos[0] || null;
-
-  for (let i = 0; i < videos.length; ++i) {
-    let rect = video.getBoundingClientRect();
-    let size = rect.height * rect.width;
-    if (size > largestVideoSize) {
-      largestVideoSize = size;
-      video = videos[i];
-    }
-  }
-  return video;
-}
-function shallowEqual(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (let key of keys1) {
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-const stopVideoStreams = () => {
-  window.LOCAL_MEDIA_STREAM?.getTracks().forEach((t) => {
-    // t.enabled = false;
-    t.stop();
-  });
-  window.LOCAL_MEDIA_STREAM = null;
-  document
-    .querySelector('#WEBROWSE_FULLSCREEN_CONTAINER')
-    .querySelectorAll('video')
-    .forEach((v) => {
-      let st = v.srcObject;
-      if (st) {
-        st.getTracks().forEach((t) => t.stop());
-      }
-    });
-};
 function createUUID() {
   // http://www.ietf.org/rfc/rfc4122.txt
   var s = [];
@@ -159,14 +43,7 @@ function createUUID() {
   return uuid;
 }
 export {
-  getVideoPlayer,
   throttle,
-  selectText,
   getUser,
-  getUsername,
-  preventCloseTabHandler,
-  getTranslateValues,
-  shallowEqual,
-  stopVideoStreams,
   createUUID
 };
