f95965e43e5dd7fc3521df1e871e90353e6b92e9
laoona <--global>
2022-01-18 13:53:51 +0800

    fixed: 修复了 Enter 键不能发送消息的 Bug

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -12,8 +12,14 @@ import stylesChat from './Webrowse/Floater/shadow.css';
 
 // 禁止与页面上的一些快捷键冲突
 const handleKeydown = (evt) => {
-  evt.stopPropagation()
+  // 排除聊天的文本域、或者按下了 Enter 键
+  if (evt.target.classList.contains('rta__textarea') || +evt.keyCode === 13) {
+    return false;
+  }
+
+  evt.stopPropagation();
 }
+
 ReactDOM.render(
   <root.div onKeyDown={handleKeydown}>
     <style type="text/css">{stylesChat}</style>
